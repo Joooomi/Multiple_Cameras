1e39e6e263bad5b1a4e7f521c29fa7cebf500b93
Joooomi <128105594+Joooomi@users.noreply.github.com>
2024-04-05 14:12:19 +0900

    Update Name_map.py
    add description

--- Name_map.py
+++ Name_map.py
@@ -1,17 +1,20 @@
+# Friendly nameでカメラを起動する
 import wmi
 import cv2
 
 def get_camera_properties():
     w = wmi.WMI()   
     
+    # Win32_PnPEntityから 'USB2.0_Camera'を探す
     cameras = w.query("SELECT * FROM Win32_PnPEntity WHERE Name LIKE '%USB2.0_Camera%'")
     
     camera_properties = []
     for camera in cameras:
+        # プロパティを取得
         name = camera.name
         description = camera.Description
         pnp_device_id = camera.PNPDeviceID
-        
+        #取得したプログラムをリストに追加
         camera_properties.append({        
             "Name": name,
             "Description": description,
@@ -21,30 +24,32 @@ def get_camera_properties():
     return camera_properties
 
 def create_camera_mapping(camera_properties):
-    
+    # Friendly nameとcamera indexのマップを作成
     mapping = {}
     for index, camera in enumerate(camera_properties):
         
-        serial_number = camera["PNPDeviceID"].split("\\")[2] #==Instance ID
+        #serial_number = camera["PNPDeviceID"].split("\\")[2] #==Instance ID
+        # Friendly nameを取得
         name = camera["Name"]
-        
+        # マップに追加
         mapping[name] = index
     
     return mapping
 
-
+# カメラのプロパティを取得
 camera_properties = get_camera_properties()
 
-
+# マップ作成
 camera_mapping = create_camera_mapping(camera_properties)
 print("Camera Mapping:", camera_mapping)
 
-
-desired_name = "USB2.0_Camera_1"
+# Friendly name で起動するカメラを選択
+desired_name = "USB2.0_Camera_Front"
 if desired_name in camera_mapping:
-    desired_index = camera_mapping[desired_name] +1 
+    desired_index = camera_mapping[desired_name] +1 #内臓カメラがインデクス0になっているため、１を足す
     print("Desired Camera Index:", desired_index)    
-    
+
+    # 作成したマップから取得したカメラのインデクスでカメラを開く
     cap = cv2.VideoCapture(desired_index, cv2.CAP_DSHOW)
     
     if not cap.isOpened():
